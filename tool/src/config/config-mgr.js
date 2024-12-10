13e3a8bdbd8f915d7f4a8539faef1df7537f5a38
HieuDang <trhieu1247@gmail.com>
2024-12-10 21:48:53 +1100

    configuration function update

--- tool/src/config/config-mgr.js
+++ tool/src/config/config-mgr.js
@@ -1,14 +1,15 @@
 const chalk = require('chalk');
-const pkgUp = require('pkg-up');
+const { cosmiconfigSync } = require('cosmiconfig');
+const configLoader = cosmiconfigSync('tool');
 
 module.exports = function getConfig() {
-	const pkgPath = pkgUp.sync({cwd: process.cwd()});
-	const pkg = require(pkgPath);
-	if (pkg.tool) {
-		console.log('Found configuration');
-		return pkg.tool;
-	} else {
+	const result = configLoader.search(process.cwd());
+	if (!result) {
 		console.log(chalk.yellow('Could not find the configuration, using default.'));
 		return { port: 1234 }
+		
+	} else {
+		console.log('Found this configuration', result.config);
+		return result.config;
 	}
 }
