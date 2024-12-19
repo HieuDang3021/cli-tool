fc2e45d9b6243506c9cf61ce0cb095d818e84244
HieuDang <trhieu1247@gmail.com>
2024-12-19 22:08:34 +1100

    add logger

--- tool/src/config/config-mgr.js
+++ tool/src/config/config-mgr.js
@@ -1,3 +1,4 @@
+const logger = require('../logger')('config:mgr');
 const chalk = require('chalk');
 const { cosmiconfigSync } = require('cosmiconfig');
 const configLoader = cosmiconfigSync('tool');
@@ -8,18 +9,18 @@ const ajv = new Ajv();
 module.exports = function getConfig() {
 	const result = configLoader.search(process.cwd());
 	if (!result) {
-		console.log(chalk.yellow('Could not find the configuration, using default.'));
+		logger.warning('Could not find the configuration, using default.');
 		return { port: 1234 }
 		
 	} else {
 		const isValid = ajv.validate(schema, result.config);
 		if (!isValid) {
-			console.log(chalk.yellow('Invalid configuration provided!'));
+			logger.warning('Invalid configuration provided!');
 			console.log();
 			console.log(ajv.errors.map((error) =>`${error.instancePath}: ${error.message}`).join('\n'));
 			process.exit(1);
 		}
-		console.log('Found this configuration', result.config);
+		logger.debug('Found this configuration', result.config);
 		return result.config;
 	}
 }
